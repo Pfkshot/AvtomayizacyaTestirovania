d57a66e8faec164d27f1afb528ce1d13e2af316b
Pfkshot <Pfkshot@gmail.com>
2024-07-23 16:48:20 +0300

    DZ_1.1

---
+++ src/main/java/org/igor/DZ_1/Door.java
@@ -0,0 +1,18 @@
+package org.igor.DZ_1;
+
+/**
+ * Класс для описания двери
+ */
+public class Door {
+
+    private boolean isPrize;
+
+    public Door(boolean isPrize) {
+        this.isPrize = isPrize;
+    }
+
+    public boolean isPrize() {
+        return isPrize;
+    }
+}
+
